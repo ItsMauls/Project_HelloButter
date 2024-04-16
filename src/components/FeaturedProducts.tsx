f74294ef3cad22c512d9c2fddb67c627e8832981
Maulana Ibrahim <maulputra09@gmail.com>
2024-04-16 09:25:26 +0700

    chores: add image source to config

--- src/components/FeaturedProducts.tsx
+++ src/components/FeaturedProducts.tsx
@@ -23,7 +23,7 @@ const getFeaturedProducts = async():Promise<Product[]> => {
               <>
               <div key={product._id.toString()} className="group relative">
               <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
-                <Image src={product.images[0]} alt={product.name} className="w-full h-full object-center object-cover lg:w-full lg:h-full" />
+                <Image height={500} width={500} src={product.images[0]} alt={product.name} className="w-full h-full object-center object-cover lg:w-full lg:h-full" />
               </div>
               <div className="mt-4 flex justify-between">
                 <div>
